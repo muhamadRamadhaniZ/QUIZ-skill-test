f7b7ffe65209f8b78bcf55d7ee0483602f4516f9
muhamadRamadhaniZ <danii251018@gmail.com>
2022-10-05 15:27:48 +0700

    quiz 2

---
+++ bilanagan_faktorial.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const rline = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+console.log('selamat di perhitungan bilangan faktorial');
+rline.question('Masukkan bilanagan? ', function (number) {
+    if (number == '') {
+        console.log('bilanagan harus diisi!');
+        rline.close();
+    }
+    let hasil = number;
+    for (let index = number-1; index > 0; index--) {
+        hasil = hasil * index;
+    }
+    console.log(`hasil faktorial bilangan ${number} adalah ${hasil}`);
+    rline.close();
+});
+
+rline.on('close', function () {
+  console.log('\nBYE BYE !!!');
+  process.exit(0);
+});
