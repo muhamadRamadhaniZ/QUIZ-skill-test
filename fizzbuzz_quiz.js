f44b5d83cc9b78be87c266523e4a4cf3df401939
muhamadRamadhaniZ <danii251018@gmail.com>
2022-10-06 15:14:52 +0700

    fix

--- fizzbuzz_quiz.js
+++ fizzbuzz_quiz.js
@@ -9,19 +9,22 @@ rline.question('Masukkan angka? ', function (number) {
         console.log('angka harus diisi!');
         rline.close();
     }
+    let hasil = "";
     for (let index = 1; index <= number; index++) {
         if (index % 3 == 0 && index % 5 == 0) {
-            console.log('fizzbuzzz');
+            hasil += "fizzbuzzz,";
         }
         else if( index % 3 == 0){
-            console.log('fizz');
+            hasil += "fizz,";
         }
         else if( index % 5 == 0   ){
-            console.log('buzzz');
+            hasil += "buzz,";
+            
         }else{
-            console.log(index);
+            hasil += index + ",";
         }
     }
+    console.log(hasil);
     rline.close();
 });
 
